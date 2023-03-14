63c4f2c989a7441c27066b6ad9b7f7f26baf254b
Juan Felipe Cardona Gasca <jfelipec@iadb.org>
2023-03-14 12:46:31 -0500

    Feat: create expanding-cards mini challenge

---
+++ expanding-cards/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var cards = document.querySelectorAll('#expanding-cards .card');
+    console.log(cards);
+    cards.forEach(card => card.addEventListener('click', toggleActive));
+});
+
+function toggleActive(event){
+    document.querySelectorAll('#expanding-cards .card').forEach(card => card.classList.remove('active'));
+    event.target.classList.add('active');
+}
